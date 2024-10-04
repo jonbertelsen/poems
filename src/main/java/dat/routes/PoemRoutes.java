fe6ff58062a827da0ef4d5400f5cc02ead5fe708
Jon Bertelsen <jobe@cphbusiness.dk>
2024-10-04 10:13:18 +0200

    add before handler to admin poem endpoints

--- src/main/java/dat/routes/PoemRoutes.java
+++ src/main/java/dat/routes/PoemRoutes.java
@@ -1,6 +1,7 @@
 package dat.routes;
 
 import dat.controllers.PoemController;
+import dat.security.controllers.SecurityController;
 import dat.security.enums.Role;
 import io.javalin.apibuilder.EndpointGroup;
 
@@ -9,13 +10,16 @@ import static io.javalin.apibuilder.ApiBuilder.*;
 public class PoemRoutes {
 
     PoemController poemController = new PoemController();
+    SecurityController securityController = SecurityController.getInstance();
 
     public EndpointGroup getRoutes(){
         return () -> {
+
+            get("/{id}", poemController::getById, Role.ANYONE);
+            before(securityController.authenticate()); // check if there is a valid token in the header
             post("/", poemController::createPoem, Role.ADMIN);
             delete("/{id}", poemController::delete, Role.ADMIN);
             put("/{id}", poemController::update, Role.ADMIN);
-            get("/{id}", poemController::getById, Role.ANYONE);
-        };
+                   };
     }
 }
