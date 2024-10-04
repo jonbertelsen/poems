9fe04ca483bf8e30bd17d2168ac163f6c73f602d
Jon Bertelsen <jobe@cphbusiness.dk>
2024-10-05 01:34:33 +0200

    put issuer and expiration time into config.properties

--- src/main/java/dat/utils/Utils.java
+++ src/main/java/dat/utils/Utils.java
@@ -21,13 +21,19 @@ public class Utils {
     public static void main(String[] args) {
         System.out.println(getPropertyValue("db.name", "properties-from-pom.properties"));
     }
-    public static String getPropertyValue(String propName, String ressourceName)  {
+    public static String getPropertyValue(String propName, String resourceName)  {
         // REMEMBER TO BUILD WITH MAVEN FIRST. Read the property file if not deployed (else read system vars instead)
         // Read from ressources/config.properties or from pom.xml depending on the ressourceName
-        try (InputStream is = Utils.class.getClassLoader().getResourceAsStream(ressourceName)) { //"config.properties" or "properties-from-pom.properties"
+        try (InputStream is = Utils.class.getClassLoader().getResourceAsStream(resourceName)) {
             Properties prop = new Properties();
             prop.load(is);
-            return prop.getProperty(propName);
+
+            String value = prop.getProperty(propName);
+            if (value != null) {
+                return value.trim();  // Trim whitespace
+            } else {
+                throw new ApiException(500, String.format("Property %s not found in %s", propName, resourceName));
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
             throw new ApiException(500, String.format("Could not read property %s. Did you remember to build the project with MAVEN?", propName));
